574976bd5895d583fb88becc75cd672b01d537e1
Awais Tahir <atahir@sqlpk.com>
2024-08-13 16:35:49 +0500

    Add some test changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Hello World 2
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
